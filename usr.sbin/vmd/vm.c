50a233cd8db3bbd1ece3ccde56f926772c038844
David Montes <david.montes@sjsu.edu>
2020-04-08 12:59:35 -0700

    printing vibResult

--- usr.sbin/vmd/vm.c
+++ usr.sbin/vmd/vm.c
@@ -266,11 +266,16 @@ vm_balloon_thread_fn(void *p)
 		if (temp == -1) {
 			log_warn("balloon ioctl failed: %s",
 			    strerror(errno));
-		} else if (vib.vib_host_is_swapping) {
-			//vib_host_is_swapping contains the number of pages to swap
-			// assume vm has number of pages to swap
-			log_debug("host in swap, requesting inflate");
-			viombh_do_inflate(vm);
+		} else 
+			int vibResult = vib.vib_host_is_swapping;
+			printf("%d vibResult: \n", vibResult);
+
+			if (vibResult) {
+				//vib_host_is_swapping contains the number of pages to swap
+				// assume vm has number of pages to swap
+				printf("host in swap, requesting inflate \n");
+				viombh_do_inflate(vm);
+			}
 		}
 	}
 
